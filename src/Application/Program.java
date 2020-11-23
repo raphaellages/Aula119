fb80ac7780b8261d89d37057d230c2422ea5fe7c
raphaellages <74679593+raphaellages@users.noreply.github.com>
2020-11-23 10:55:43 -0300

    Add files via upload
    Aula iniciada no mac

---
+++ src/Application/Program.java
@@ -0,0 +1,16 @@
+package Application;
+
+import java.text.SimpleDateFormat;
+
+import Entities.Post;
+
+public class Program {
+
+	public static void main(String[] args) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Post p1 = new Post();
+		Post p2 = new Post()
+		
+	}
+
+}
---
+++ src/Entities/comment.java
@@ -0,0 +1,22 @@
+package Entities;
+
+public class comment {
+private String comment;
+
+public comment(String comment) {
+	this.comment = comment;
+}
+
+public comment() {
+}
+
+public String getComment() {
+	return comment;
+}
+
+public void setComment(String comment) {
+	this.comment = comment;
+}
+
+
+}
